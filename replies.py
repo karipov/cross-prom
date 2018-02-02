f556a6e925921f7d0f206ea37d18f670247f9ba9
karipov <aripovkamron@gmail.com>
2018-02-02 23:42:55 +0100

    added enter_chan_error and dots to replies

--- replies.py
+++ replies.py
@@ -3,14 +3,15 @@ help_reply = "Check me out; I'm <a href='https://github.com/karipov/cross-prom'>
 
 #SERVICE RESPONSES
 success_add = "{} was successfully added to the list!"
-trunc_list = "The list has been truncated"
-terminate_bot = "Bot has been terminated"
+trunc_list = "The list has been truncated."
+terminate_bot = "Bot has been terminated."
 size_set = "Minimum channel size has been set to {} members."
 
 #ERRORS
 enter_chan = "Please enter a channel."
 long_name = "The name is too long."
 no_chans = "No channels are available."
-admin_only = "This is an admin-only command"
+admin_only = "This is an admin-only command."
 small_chan = "Your channel is too small. The minimum size is {}."
-size_set_error = "Please enter a number after this command"
+size_set_error = "Please enter a number after this command."
+enter_chan_error = "Please enter a channel name after this command."
